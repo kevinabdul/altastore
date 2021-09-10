f92a5fd2c3364b45edc623533e3d4dcfe49f8b63
kevinabdul <kevin.abdulfattah1@gmail.com>
2021-09-10 14:54:33 +0700

    add post checkout feature

--- controllers/checkout/checkoutController.go
+++ controllers/checkout/checkoutController.go
@@ -26,3 +26,29 @@ func GetCheckoutByUserIdController(c echo.Context) error {
 		Data 	models.CheckoutAPI
 	}{Status: "success", Data: checkout})
 }
+
+func AddCheckoutByUserIdController(c echo.Context) error {
+	userId , _ := strconv.Atoi(c.Request().Header.Get("userId"))
+	payment := models.PaymentAPI{}
+	c.Bind(&payment)
+	invoiceId, rowsAffected, err := libdb.AddCheckoutByUserId(payment.PaymentMethod, userId)
+
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, struct {
+			Status 	string
+			Message string
+		}{Status: "failed", Message: err.Error()})
+	}
+
+	if rowsAffected == 0 {
+		return c.JSON(http.StatusBadRequest, struct {
+			Status 	string
+			Message string
+		}{Status: "failed", Message: "fail to create invoice!"})
+	}
+
+	return c.JSON(http.StatusOK, struct {
+		Status 		string
+		InvoiceID	string
+	}{Status: "success", InvoiceID: invoiceId})
+}
